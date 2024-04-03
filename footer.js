5df1beb5bdcd8b7ebb63e140600c216801a0ca64
yash dargude <yashdargude@yashs-MacBook-Air.local>
2024-04-04 01:40:02 +0530

    new commit

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ const footerHTML = `
   <footer class="section-footer">
     <div class="footer-container container">
       <div class="content_1">
-        <img src="./images/logo.png" alt="logo" />
+        <h1 class="web-logo">TechNest</h1>
         <p>
           Welcome to Thapa EcomStore, <br />
           your ultimate destination for
